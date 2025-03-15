5d38c1e4a0f622fce9f87cac2ed553fb6c711b75
Dagim Wubeante <dagimw14@gmail.com>
2025-03-15 12:02:55 +0300

    [1] [Time Beats: 100.00%] [Memory Beats: 24.66%] - LeetPush

--- Two-Sum.py
+++ Two-Sum.py
@@ -1,14 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
-        for i in range(len(nums)):
-            complementary = target - nums[i]
-            try:
-                complementary_index = nums.index(complementary, i+1)
-                return [i, complementary_index]
-            except ValueError:
-                continue
-        return []
+        hash = {}
+        for index, num in enumerate(nums):
+            complement = target - num
+            if complement in hash:
+                return [hash[complement], index]
+            hash[num] = index
+            
                 
 
 
